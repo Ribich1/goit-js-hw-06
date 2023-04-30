7ef68430c770194f93335e10361ce399a66ebd7f
Ribich1 <ribich@ukr.net>
2023-04-30 13:46:21 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const arrLi = [];
+ingredients.forEach(function (num) {
+  let li = "";
+  li = document.createElement("li");
+li.textContent = num;
+li.className = "item";
+  arrLi.push(li)
+});
+console.log(arrLi);
+
+const element = document.querySelector("#ingredients")
+element.append(...arrLi);
