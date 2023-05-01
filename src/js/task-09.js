bae92c0482ef35ef9037026a4632de4c2c58bdaf
Ribich1 <ribich@ukr.net>
2023-05-01 20:14:17 +0200

    Task09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
+const divEl = document.querySelector(".widget");
+
+
+btnEl.addEventListener('click', onChangeColor);
+
+console.log(getRandomHexColor());
+
+
+function onChangeColor(event) {
+  const color = getRandomHexColor();
+  nameColor.textContent = color;
+  divEl.style.backgroundColor = color;
+}
