60f0c16c0b5b3853d3bad0220e8c2a84744d9c02
Ribich1 <ribich@ukr.net>
2023-05-01 18:34:08 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+
+form.addEventListener('submit',onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+        
+        if (value === '') {
+            const alert = console.log("заполни все поля!");
+            return alert;
+        } else
+            console.log(`${name}: ${value}`);
+    })
+    
+
+}
