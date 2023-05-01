f70e5f3e5eb224f1901332c28eab7c560ce976ed
Ribich1 <ribich@ukr.net>
2023-05-01 16:14:51 +0200

    Task05 finish

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,15 +1,15 @@
-const listCatWithId = document.querySelectorAll("#categories");
+const listCatWithId = document.getElementById("categories");
 const listWithClass = document.querySelectorAll(".item");
   console.log(`Number of categories: ${listWithClass.length}`);
-
-// const firstLiEl = listWithClass.firstElementChild;
-// console.dir(listWithClass[0].textContent);
 
 
 listWithClass.forEach(function (num) {
     console.log(`Category: ${num.firstElementChild.textContent}`);
     console.log(`Elements: ${num.lastElementChild.children.length}`);
 });
+
+// const firstLiEl = listWithClass.firstElementChild;
+// console.dir(listWithClass[0].textContent);
 
 
 
