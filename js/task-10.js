dce29d0d3c1456230efc10e14bf476cb6ddfc951
Ribich1 <ribich@ukr.net>
2023-05-04 01:33:41 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -7,24 +7,32 @@ function getRandomHexColor() {
 
 const divEl = document.getElementById("boxes");
 
+const arrEl = document.getElementById("controls");
+const inputEl = arrEl.firstElementChild;
+const destroy = arrEl.lastElementChild;
+const create = destroy.previousElementSibling;
+
+destroy.addEventListener('click', destroyBoxes);
+create.addEventListener("click", createBoxes)
 
 function createBoxes(amount) {
-  let q = 30; 
+  let size = 30; 
+  amount = inputEl.value;
   const fragment = document.createDocumentFragment();
   for (let i = 1; i <= amount; i += 1){
     
     let colorGen = getRandomHexColor();
     const divNew = document.createElement("div");
-    divNew.style.width = `${q}px`;
-    divNew.style.height = `${q}px`;
+    divNew.style.width = `${size}px`;
+    divNew.style.height = `${size}px`;
     divNew.style.backgroundColor = colorGen;
     fragment.appendChild(divNew);
-    q += 10;
+    size += 10;
   };
-  console.log(fragment);
+  divEl.append(fragment);
 };
 function destroyBoxes() {
-  
+  divEl.innerHTML = '';  
 };
 
-createBoxes(3);
+
